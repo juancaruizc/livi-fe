8224a2d5730451395495b0879fdd360db4e535df
Juan Ruiz <juancaruiz@Juancas-MacBook-Pro.local>
2021-12-29 14:27:46 -0700

    built projects section

--- livi-fe/src/Components/Projects/Projects.jsx
+++ livi-fe/src/Components/Projects/Projects.jsx
@@ -6,6 +6,48 @@ function Projects() {
     <div className="Projects">
       <div className="projectsContainer">
         <h1 className="projectsHeader">Ideas for Future Projects</h1>
+
+        <div className="projectList">
+          <div className="project">
+            <div className="circle">1</div>
+            <h3>Sleep</h3>
+            <div className="projectDescContainer">
+              <p className="projectDescription">
+                Sleep plays a huge role in weight loss and overall health. With
+                various sleep wearables offering their API. A sleep
+                tracking/optimizing part of Livi can be built.
+              </p>
+            </div>
+          </div>
+
+          <div className="project">
+            <div className="circle">2</div>
+            <h3>Exercise</h3>
+            <div className="projectDescContainer">
+              <p className="projectDescription">
+                Exercise plays a big part in health, and can act as a force
+                multiplier to a healthy diet. Exercise tracking and
+                accountability.
+              </p>
+            </div>
+          </div>
+
+          <div className="project">
+            <div className="circle">3</div>
+            <h3>Community</h3>
+            <div className="projectDescContainer">
+              <p className="projectDescription">
+                Getting put in a community can be super beneficial! A custom
+                community for patients for accountability/sharing wins can make
+                the journey to better health more awesome!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="btnContainer">
+          <button className="projectsBtn">Get Started</button>
+        </div>
       </div>
     </div>
   );
---
+++ livi-fe/src/Components/Reviews/Reviews.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Reviews.css";
+
+function Reviews() {
+  return (
+    <div className="Reviews">
+      <div className="reviewsContainer">
+        <div className="reviewsHeader">
+          <h1>See What Livi Patients Say</h1>
+          <h2 className="reviewsSubTitle">
+            These reviews are fetched from an API I built 👇
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Reviews;
