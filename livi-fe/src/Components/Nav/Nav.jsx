cd08d6196cc002ec2bf1df2530a5143639698bbf
Juan Ruiz <juancaruiz@Juancas-MacBook-Pro.local>
2021-12-29 10:52:05 -0700

    built nav bar and part of home page

---
+++ livi-fe/src/Components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import liviFavicon from "../../Assets/livi-favicon.png";
+import arrowImage from "../../Assets/arrow-image.png";
+
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <div className="logoContainer">
+        <img src={liviFavicon} className="liviFavicon" alt="livi favicon" />
+        <h1>livi</h1>
+      </div>
+      <nav className="navOptions">
+        <a href="_">For Providers</a>
+        <a href="_">Make a Referral</a>
+        <a href="_">About</a>
+        <a href="_">Contact Us</a>
+      </nav>
+      <button>
+        Get Started <img src={arrowImage} className="arrowImage" alt="arrow" />
+      </button>
+    </div>
+  );
+}
+
+export default Nav;
